ee846dda27a93d6d15bdd6efa40af6319a75922d
Zoilo <zmercedes89@gmail.com>
2018-12-26 13:59:38 -0500

    Feature: Swordblader class
    class_Swordblader.prefab
    - Renamed the default Player prefab to class_Swordblader, and reworked it to use SwordActions.
    
    SwordActions.cs
    - This script derives from UnitActions and handles the actions unique to the Swordblader class.

---
+++ Assets/Scripts/Character/SwordActions.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class SwordActions : UnitActions {
+
+	// weapon collider gameobject
+	GameObject weaponCollider;
+
+	// Swing animation times
+	float windUpTime = 0.12f;
+	float spinTime = 0.12f;
+	float recoveryTime = 0.25f;
+
+	public override void Awake() {
+		base.Awake();
+		weaponCollider = transform.GetChild(0).GetChild(0).GetChild(1).gameObject;
+	}
+
+	public override IEnumerator Attack(){
+		if(isLocalPlayer)
+			up = player.up;
+
+		isAttacking = true;
+		weaponCollider.SetActive(!isLocalPlayer);
+		Quaternion initial = player.rotation;
+		Quaternion from = player.rotation * Quaternion.Euler(transform.forward * 30f);
+		Quaternion to = player.rotation * Quaternion.Euler(transform.forward * -120f);
+
+		float elapsed = 0f;
+
+		while(elapsed < windUpTime){
+			player.rotation = Quaternion.Slerp(initial, from, elapsed/windUpTime);
+			elapsed += Time.fixedDeltaTime;
+			yield return null;
+		}
+		elapsed = 0f;
+
+		while(elapsed < spinTime){
+			player.rotation = Quaternion.Slerp(from, to, elapsed/spinTime);
+			elapsed += Time.fixedDeltaTime;
+			yield return null;
+		}
+		elapsed = 0f;
+
+		while(elapsed < recoveryTime){
+			player.rotation = Quaternion.Slerp(to, initial, elapsed/recoveryTime);
+			elapsed += Time.fixedDeltaTime;
+			yield return null;
+		}
+
+		player.rotation = initial;
+		isAttacking = false;
+		weaponCollider.SetActive(false);
+	}
+}
