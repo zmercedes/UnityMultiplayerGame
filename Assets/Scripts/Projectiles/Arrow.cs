2469bc2712ff56769ccb47cadf9e3c7498a26d87
zmercedes <zmercedes89@gmail.com>
2019-01-25 10:32:17 -0500

    Fix: Arrow movement
    Because the arrow container is a child of the player prefab and parent to arrows, the arrows traveled in sync with the player prefab. This commit fixes that by making the arrow have no parent during travel and reparents them upon deactivation.
    
    Arrow.cs
    - Created to handle arrow behavior.
    
    DamagePushback.cs
    - Removed arrow logic from here.
    
    Arrow.prefab
    - Updated with arrow behavior.

---
+++ Assets/Scripts/Projectiles/Arrow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Arrow : MonoBehaviour {
+
+	Transform parent;
+
+	void Awake() {
+		parent = transform.parent;
+	}
+
+	void OnEnable(){
+		transform.SetParent(null);		
+	}
+	
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.gameObject.tag != "Collect"){
+			transform.SetParent(parent);
+			gameObject.SetActive(false);
+		}
+	}
+}
